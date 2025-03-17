432e0c5bc743305b406f447268190ccc51630b0f
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-16 22:12:04 -0300

    Refactor el componente Form para mejorar la legibilidad del código al agregar paréntesis a las funciones de flecha en los controladores onChange. Se implementa la gestión del estado de favoritos en el componente MatchingPosts, permitiendo agregar y eliminar publicaciones favoritas mediante el uso de useState.

--- exercises/03.lifting-state/02.problem.lift-array/index.tsx
+++ exercises/03.lifting-state/02.problem.lift-array/index.tsx
@@ -34,18 +34,18 @@ function App() {
 function Form({
 	query,
 	setQuery,
-}: {
+}: Readonly<{
 	query: string
 	setQuery: (query: string) => void
-}) {
-	const words = query.split(' ').map(w => w.trim())
+}>) {
+	const words = query.split(' ').map((w) => w.trim())
 
 	const dogChecked = words.includes('dog')
 	const catChecked = words.includes('cat')
 	const caterpillarChecked = words.includes('caterpillar')
 
 	function handleCheck(tag: string, checked: boolean) {
-		const newWords = checked ? [...words, tag] : words.filter(w => w !== tag)
+		const newWords = checked ? [...words, tag] : words.filter((w) => w !== tag)
 		setQuery(newWords.filter(Boolean).join(' ').trim())
 	}
 
@@ -62,7 +62,7 @@ function Form({
 					name="query"
 					type="search"
 					value={query}
-					onChange={e => setQuery(e.currentTarget.value)}
+					onChange={(e) => setQuery(e.currentTarget.value)}
 				/>
 			</div>
 			<div>
@@ -70,7 +70,7 @@ function Form({
 					<input
 						type="checkbox"
 						checked={dogChecked}
-						onChange={e => handleCheck('dog', e.currentTarget.checked)}
+						onChange={(e) => handleCheck('dog', e.currentTarget.checked)}
 					/>{' '}
 					🐶 dog
 				</label>
@@ -78,7 +78,7 @@ function Form({
 					<input
 						type="checkbox"
 						checked={catChecked}
-						onChange={e => handleCheck('cat', e.currentTarget.checked)}
+						onChange={(e) => handleCheck('cat', e.currentTarget.checked)}
 					/>{' '}
 					🐱 cat
 				</label>
@@ -86,7 +86,9 @@ function Form({
 					<input
 						type="checkbox"
 						checked={caterpillarChecked}
-						onChange={e => handleCheck('caterpillar', e.currentTarget.checked)}
+						onChange={(e) =>
+							handleCheck('caterpillar', e.currentTarget.checked)
+						}
 					/>{' '}
 					🐛 caterpillar
 				</label>
@@ -96,50 +98,56 @@ function Form({
 	)
 }
 
-function MatchingPosts({ query }: { query: string }) {
+function MatchingPosts({ query }: Readonly<{ query: string }>) {
 	const matchingPosts = getMatchingPosts(query)
-	// 🐨 lift the favorite state from the Card component to here
+	const [favorites, setFavorites] = useState<Array<string>>([])
 
 	return (
 		<ul className="post-list">
 			{matchingPosts
 				.sort((a, b) => {
-					// 🐨 determine whether post a and b are included in favorites
-					const aFav = false // 💰 favorites.includes(a.id)
-					const bFav = false // 💰 favorites.includes(b.id)
+					const aFav = favorites.includes(a.id)
+					const bFav = favorites.includes(b.id)
 					return aFav === bFav ? 0 : aFav ? -1 : 1
 				})
-				.map(post => (
+				.map((post) => (
 					<Card
 						key={post.id}
 						post={post}
-						// 🐨 pass an isFavorited prop
-						// 🐨 pass an onFavoriteClick that accepts a "favorite" boolean
-						//   if it's true, then add the post.id to the favorites
-						//   if it's false, then remove the post.id from the favorites
+						isFavorited={favorites.includes(post.id)}
+						onFavoriteClick={(favorite) => {
+							if (favorite) {
+								setFavorites([...favorites, post.id])
+							} else {
+								setFavorites(favorites.filter((fav) => fav !== post.id))
+							}
+						}}
 					/>
 				))}
 		</ul>
 	)
 }
 
-// 🐨 add props for isFavorited and onFavoriteClick
-function Card({ post }: { post: BlogPost }) {
-	// 🐨 lift this up to MatchingPosts
-	const [isFavorited, setIsFavorited] = useState(false)
+function Card({
+	post,
+	isFavorited,
+	onFavoriteClick,
+}: Readonly<{
+	post: BlogPost
+	isFavorited: boolean
+	onFavoriteClick: (isFavorited: boolean) => void
+}>) {
 	return (
 		<li>
 			{isFavorited ? (
 				<button
 					aria-label="Remove favorite"
-					// 🐨 call onFavoriteClick
-					onClick={() => setIsFavorited(false)}
+					onClick={() => onFavoriteClick(false)}
 				>
 					❤️
 				</button>
 			) : (
-				// 🐨 call onFavoriteClick
-				<button aria-label="Add favorite" onClick={() => setIsFavorited(true)}>
+				<button aria-label="Add favorite" onClick={() => onFavoriteClick(true)}>
 					🤍
 				</button>
 			)}
@@ -149,7 +157,7 @@ function Card({ post }: { post: BlogPost }) {
 			/>
 			<a
 				href={post.id}
-				onClick={event => {
+				onClick={(event) => {
 					event.preventDefault()
 					alert(`Great! Let's go to ${post.id}!`)
 				}}
