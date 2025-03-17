8c0021612a8cee6bddd64051080c77ff958f9b8d
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-17 14:30:38 -0300

    Implement the use of useEffect in the Board component to manage the local storage of squares. The initialization of the squares’ state is improved by using a return function in useState, ensuring the value is correctly updated in localStorage when it changes. Code readability is enhanced by simplifying the error handling logic in JSON parsing.

--- exercises/06.tic-tac-toe/02.problem.local-storage/index.tsx
+++ exercises/06.tic-tac-toe/02.problem.local-storage/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import {
 	calculateNextValue,
@@ -8,18 +8,22 @@ import {
 } from '#shared/tic-tac-toe-utils'
 
 const defaultState = Array(9).fill(null)
-// 🐨 create a variable for the key you'll use for storing the squares
-// 💰 'squares' should work well.
-function Board() {
-	// 🐨 use the callback form for useState. The callback should:
-	// 1. get the value from localStorage using the key you created above
-	// 2. parse the JSON from that value
-	// 3. return the parsed value (or the default value if there isn't one)
-	// 💯 for extra credit, handle situations where the value doesn't exist or fails to parse
-	const [squares, setSquares] = useState<Squares>(Array(9).fill(null))
+const key = 'squares'
 
-	// 🐨 add a useEffect here that updates the local storage value of the squares
-	// 💰 you should stringify the squares using JSON.stringify because local storage only supports strings
+function Board() {
+	const [squares, setSquares] = useState<Squares>(() => {
+		const squares = localStorage.getItem(key)
+		if (!squares) return defaultState
+		try {
+			return JSON.parse(squares)
+		} catch {
+			return defaultState
+		}
+	})
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(squares))
+	}, [squares])
 
 	const nextValue = calculateNextValue(squares)
 	const winner = calculateWinner(squares)
@@ -27,7 +31,7 @@ function Board() {
 
 	function selectSquare(index: number) {
 		if (winner || squares[index]) return
-		setSquares(previousSquares => previousSquares.with(index, nextValue))
+		setSquares((previousSquares) => previousSquares.with(index, nextValue))
 	}
 
 	function restart() {
