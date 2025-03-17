988cd378681e73009d72dae19b2e1f147c6c58ce
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-17 15:12:21 -0300

    Refactor the Board component to manage the game state using a GameState object. The square selection and reset functions are updated, improving code readability by removing unnecessary references and simplifying state management logic. Additionally, local storage is adjusted to save the complete game state.

--- exercises/06.tic-tac-toe/03.problem.history/index.tsx
+++ exercises/06.tic-tac-toe/03.problem.history/index.tsx
@@ -4,9 +4,8 @@ import {
 	calculateNextValue,
 	calculateStatus,
 	calculateWinner,
-	// 💰 these could be handy
-	// isValidGameState,
-	// type GameState,
+	isValidGameState,
+	type GameState,
 	type Squares,
 } from '#shared/tic-tac-toe-utils'
 
@@ -49,14 +48,14 @@ function Board({
 	)
 }
 
-// 🐨 our new default state will be a GameState object
-const defaultState = Array(9).fill(null)
+const defaultState: GameState = {
+	history: [Array(9).fill(null)],
+	currentStep: 0,
+}
 
-// 🐨 probably makes sense to change the name of the localStorageKey to 'tic-tac-toe'
-const localStorageKey = 'squares'
+const localStorageKey = 'tic-tac-toe'
 function App() {
-	// 🐨 You can now call this simply "state" and "setState" and it's now GameState instead of Squares
-	const [squares, setSquares] = useState<Squares>(() => {
+	const [state, setState] = useState<GameState>(() => {
 		let localStorageValue
 		try {
 			localStorageValue = JSON.parse(
@@ -66,56 +65,64 @@ function App() {
 			// something is wrong in localStorage, so don't use it
 		}
 
-		// 🐨 you now need to make sure it's a valid game state object.
-		// 💰 isValidGameState(localStorageValue) will do that for you
-		return localStorageValue && Array.isArray(localStorageValue)
+		return isValidGameState(localStorageValue)
 			? localStorageValue
 			: defaultState
 	})
 
-	// 🐨 get the "currentSquares" from state.history[state.currentStep]
+	const currentSquares = state.history[state.currentStep]
 
-	// 🐨 any reference to "squares" below should be changed to "currentSquares"
-	const winner = calculateWinner(squares)
-	const nextValue = calculateNextValue(squares)
-	const status = calculateStatus(winner, squares, nextValue)
+	const winner = calculateWinner(currentSquares)
+	const nextValue = calculateNextValue(currentSquares)
+	const status = calculateStatus(winner, currentSquares, nextValue)
 
 	useEffect(() => {
-		// 🐨 we should serialize the entire state here instead of just the squares
-		window.localStorage.setItem(localStorageKey, JSON.stringify(squares))
-		// 🐨 update the dependency array to be state instead of squares
-	}, [squares])
+		window.localStorage.setItem(localStorageKey, JSON.stringify(state))
+	}, [state])
 
 	function selectSquare(index: number) {
-		if (winner || squares[index]) return
-		// 🐨 this is now setState and previousState, not setSquares and previousSquares
-		setSquares(previousSquares => {
-			// 🐨 create an updated history and squares object
-			// 💰 note that the history should be from index 0 to the current step plus the new squares
-			// and the new current step should be equal to the last index of the new history
-			return previousSquares.with(index, nextValue)
+		if (winner || currentSquares[index]) return
+
+		setState((previousState) => {
+			const { currentStep, history } = previousState
+			const newHistory = history.slice(0, currentStep + 1)
+			const squares = history[currentStep].with(index, nextValue)
+
+			return {
+				history: [...newHistory, squares],
+				currentStep: newHistory.length,
+			}
 		})
 	}
 
 	function restart() {
-		// 🐨 this is now setState
-		setSquares(defaultState)
+		setState(defaultState)
 	}
 
-	// 🐨 create moves by mapping over the history and rendering on <li> for each
-	// step in the history. This should have a button which when clicked calls
-	// `setState` to update the currentStep.
-	// 💯 disable the current step
-	// 💰 NOTE: the "step" is actually the "index" which normally you don't want to
-	// use as the "key" prop. However, in this case, the index is effectively
-	// the "id" of the step in history, so it is correct.
-	const moves = 'TODO: create moves li elements'
+	const moves = state.history.map((_stepSquares, step) => {
+		const desc = step ? `Go to move number ${step}` : 'Go to game start'
+		const isCurrentStep = step === state.currentStep
+		return (
+			<li key={step}>
+				<button
+					onClick={() =>
+						setState((previousState) => ({
+							...previousState,
+							currentStep: step,
+						}))
+					}
+					disabled={isCurrentStep}
+				>
+					{desc} {isCurrentStep ? '(current)' : null}
+				</button>
+			</li>
+		)
+	})
 
 	return (
 		<div className="game">
 			<div className="game-board">
-				{/* 🐨 update the reference of "squares" here to "currentSquares" */}
-				<Board onClick={selectSquare} squares={squares} />
+				<Board onClick={selectSquare} squares={currentSquares} />
 				<button className="restart" onClick={restart}>
 					restart
 				</button>
