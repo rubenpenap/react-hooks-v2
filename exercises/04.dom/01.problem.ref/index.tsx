3e5d0dca06c4e9b4cfe03aa956841c9bfd6aa791
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-17 13:48:22 -0300

    Refactor the Tilt component to initialize VanillaTilt on the reference node and improve code readability by adding parentheses to arrow functions in event handlers.

--- exercises/04.dom/01.problem.ref/index.tsx
+++ exercises/04.dom/01.problem.ref/index.tsx
@@ -1,30 +1,27 @@
 import { useState } from 'react'
 import { createRoot } from 'react-dom/client'
-// 💰 you'll need this stuff:
-// import VanillaTilt from 'vanilla-tilt'
-//
-// interface HTMLVanillaTiltElement extends HTMLDivElement {
-// 	vanillaTilt?: VanillaTilt
-// }
-//
-// const vanillaTiltOptions = {
-// 	max: 25,
-// 	speed: 400,
-// 	glare: true,
-// 	'max-glare': 0.5,
-// }
+import VanillaTilt from 'vanilla-tilt'
+
+interface HTMLVanillaTiltElement extends HTMLDivElement {
+	vanillaTilt?: VanillaTilt
+}
+
+const vanillaTiltOptions = {
+	max: 25,
+	speed: 400,
+	glare: true,
+	'max-glare': 0.5,
+}
 
 function Tilt({ children }: { children: React.ReactNode }) {
 	return (
 		<div
 			className="tilt-root"
-			// 🐨 add a ref callback here
-			// the callback should accept a tiltNode parameter (🦺 typed as an
-			// HTMLVanillaTiltElement) and then:
-			// - if tiltNode is null, return
-			// - call VanillaTilt.init(tiltNode, vanillaTiltOptions)
-			// - return a cleanup function that will be called when element is removed
-			//   - call tiltNode.vanillaTilt?.destroy()
+			ref={(tiltNode: HTMLVanillaTiltElement) => {
+				if (!tiltNode) return
+				VanillaTilt.init(tiltNode, vanillaTiltOptions)
+				return () => tiltNode.vanillaTilt?.destroy()
+			}}
 		>
 			<div className="tilt-child">{children}</div>
 		</div>
@@ -36,13 +33,13 @@ function App() {
 	const [count, setCount] = useState(0)
 	return (
 		<div>
-			<button onClick={() => setShowTilt(s => !s)}>Toggle Visibility</button>
+			<button onClick={() => setShowTilt((s) => !s)}>Toggle Visibility</button>
 			{showTilt ? (
 				<Tilt>
 					<div className="totally-centered">
 						<button
 							className="count-button"
-							onClick={() => setCount(c => c + 1)}
+							onClick={() => setCount((c) => c + 1)}
 						>
 							{count}
 						</button>
